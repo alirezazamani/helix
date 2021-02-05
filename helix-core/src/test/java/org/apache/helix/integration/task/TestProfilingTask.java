17117d6cac7aab92fb8c4a177daa698c05dd4921
Ali Reza Zamani Zadeh Najari <anajari@linkedin.com>
2021-02-05 11:41:02 -0800

    Just write context

--- helix-core/src/test/java/org/apache/helix/integration/task/TestProfilingTask.java
+++ helix-core/src/test/java/org/apache/helix/integration/task/TestProfilingTask.java
@@ -43,6 +43,7 @@ import org.apache.helix.task.TaskUtil;
 import org.apache.helix.task.Workflow;
 import org.apache.helix.task.WorkflowConfig;
 import org.apache.helix.task.WorkflowContext;
+import org.apache.helix.zookeeper.datamodel.ZNRecord;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -59,7 +60,6 @@ public class TestProfilingTask extends TaskTestBase {
   public void beforeClass() throws Exception {
     _numNodes = 3;
     super.beforeClass();
-    _controller.syncStop();
   }
 
   @AfterClass
@@ -73,28 +73,45 @@ public class TestProfilingTask extends TaskTestBase {
     String generatedString = RandomStringUtils.random(60, true, true);
 
     String workflowName = TestHelper.getTestMethodName();
-    String jobName = generatedString;
+    String jobName = RandomStringUtils.random(200, true, true);
 
     JobConfig.Builder jobBuilder1 = new JobConfig.Builder().setWorkflow(workflowName)
         .setNumberOfTasks(1).setNumConcurrentTasksPerInstance(100).setCommand(MockTask.TASK_COMMAND)
-        .setDisableExternalView(false).setExpiry(9223372036854774L).setFailureThreshold(322).setMaxAttemptsPerTask(2147483647).setMaxAttemptsPerTask(2147483647).setRebalanceRunningTask(false).setTimeoutPerTask(9223372036854774000L)
-        .setIgnoreDependentJobFailure(false)
-        .setJobCommandConfigMap(ImmutableMap.of(MockTask.JOB_DELAY, "99999999"));
+        .setJobCommandConfigMap(ImmutableMap.of(MockTask.JOB_DELAY, "9999999999999"));
 
     Workflow.Builder workflowBuilder1 =
         new Workflow.Builder(workflowName).addJob(jobName, jobBuilder1);
     _driver.start(workflowBuilder1.build());
 
-    // Add short running task
-    for (int i = 1; i < 10000; i++) {
-      Map<String, String> newTaskConfig =
-          new HashMap<String, String>(ImmutableMap.of("TASK_SUCCESS_OPTIONAL", "true", RandomStringUtils.random(38, true, true),
-              RandomStringUtils.random(273, true, true), "job.name", generatedString, "task.id",
-              generatedString + "_" + RandomStringUtils.random(3, false, true)));
-      TaskConfig task = new TaskConfig(null, newTaskConfig, null, null);
-      _driver.addTask(workflowName, jobName, task);
-      System.out.println(i + " " + serializer.serialize(_driver.getJobConfig(TaskUtil.getNamespacedJobName(workflowName,jobName)).getRecord()).length);
+    _driver.pollForJobState(workflowName, TaskUtil.getNamespacedJobName(workflowName, jobName),
+        TaskState.IN_PROGRESS);
+
+    Thread.sleep(10000L);
+    _controller.syncStop();
+
+    String contextPathP0 = _manager.getHelixDataAccessor().keyBuilder().jobContextZNode(workflowName, jobName).toString();
+
+    ZNRecord record = new ZNRecord(jobName);
+    record.setSimpleField("EXECUTION_START_TIME", "1612486800285");
+    record.setSimpleField("FINISH_TIME", "1612486868549");
+    record.setSimpleField("NAME", "jobName");
+    record.setSimpleField("START_TIME", "1612486800283");
+    for (int i=0 ; i< 1000000; i++) {
+      Map<String, String> temp = new HashMap<>();
+      temp.put("ASSIGNED_PARTICIPANT", RandomStringUtils.random(40, true, true));
+      temp.put("FINISH_TIME", RandomStringUtils.random(13, false, true));
+      temp.put("INFO", "");
+      temp.put("NUM_ATTEMPTS", "1");
+      temp.put( "START_TIME", RandomStringUtils.random(13, false, true));
+      temp.put( "STATE", "COMPLETED");
+      temp.put("TARGET", RandomStringUtils.random(20, true, true));
+      record.setMapField(Integer.toString(i),temp);
+      _manager.getHelixDataAccessor().getBaseDataAccessor().set(contextPathP0, record,
+          AccessOption.PERSISTENT);
+
+      System.out.println(i + " " + serializer
+          .serialize(_driver.getJobContext(TaskUtil.getNamespacedJobName(workflowName, jobName))
+              .getRecord()).length);
     }
-    Thread.sleep(500000000000L);
   }
 }
